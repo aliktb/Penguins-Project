53a46b6d4e7084dfa81ef5b432b68958c1379563
aliktb <ali.khattab1@pm.me>
2021-11-05 13:24:57 +0000

    added alerts to update page

--- update.js
+++ update.js
@@ -7,6 +7,35 @@ let hasHappyFeetInputUpdated = document.getElementsByName(
   "hasHappyFeetInputUpdated"
 );
 let updateButton = document.querySelector("#updateButton");
+let alertUpdateDiv = document.querySelector("#alertUpdateDiv");
+
+let addSuccessUpdateMessage = (object) => {
+  let newDiv = document.createElement("div");
+
+  newDiv.classList = "alert alert-success col-sm mt-4 new-alert";
+
+  newDiv.innerHTML = `<strong>Success!</strong> Penguin <strong>${object.name}</strong> has been updated!`;
+
+  alertUpdateDiv.appendChild(newDiv);
+
+  setTimeout(function () {
+    $(".new-alert").fadeOut(400);
+  }, 2000);
+};
+
+let addFailUpdateMessage = () => {
+  let newDiv = document.createElement("div");
+
+  newDiv.classList = "alert alert-danger col-sm mt-4 new-alert";
+
+  newDiv.innerHTML = `<strong>Error!</strong> An error has occured!`;
+
+  alertUpdateDiv.appendChild(newDiv);
+
+  setTimeout(function () {
+    $(".new-alert").fadeOut(400);
+  }, 2000);
+};
 
 let createUpdatedObject = () => {
   let idVal = idToUpdateInput.value;
@@ -36,9 +65,16 @@ let updateFuntion = (idToUpdate, updatedObject) => {
     headers: {
       "Content-type": "application/json; charset=UTF-8",
     },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  }).then((response) => {
+    if (response.status < 200 || response.status > 299) {
+      console.error(`status: ${response.status}`);
+      addFailUpdateMessage();
+      console.log(`There was an error trying to update`);
+      return;
+    }
+    addSuccessUpdateMessage(updatedObject);
+    response.json().then((json) => console.log(json));
+  });
 };
 
 updateButton.addEventListener("click", createUpdatedObject);
